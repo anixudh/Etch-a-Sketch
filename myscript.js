d6a15d1d66429e689f2bbc91a1e4c1a30f99577c
anixudh <anirudh.varma196@gmail.com>
2021-09-15 13:08:42 +0530

    added a reset button

--- myscript.js
+++ myscript.js
@@ -1,16 +1,35 @@
 const gridbox=document.querySelector(".gridbox");
 
-for(let i=0;i<16;i++){
-    for(let j=0;j<16;j++){
-        const grid=document.createElement("div");
-        grid.classList.add("grid");
-        gridbox.appendChild(grid);
+const createNewGrid=(n)=>{
+    for(let i=0;i<n;i++){
+        const gridRow=document.createElement("div");
+        for(let j=0;j<n;j++){
+            const grid=document.createElement("div");
+            grid.classList.add("grid");
+            gridRow.appendChild(grid);
+        }
+        gridRow.classList.add("gridrow");
+        gridbox.appendChild(gridRow);
     }
 }
+
+const resetGrid=e=>{
+    const gridrows=document.querySelectorAll(".gridrow");
+    const newSize=prompt("Enter new grid size");
+    gridrows.forEach(gridRow=>gridbox.removeChild(gridRow));
+    createNewGrid(newSize);
+    const grids=document.querySelectorAll(".grid");
+    grids.forEach(grid=>grid.addEventListener('mouseenter',colorGrid));
+};
 
 const colorGrid=e=>{
     e.target.classList.add("colorGrid");
 };
+
+createNewGrid(16);
+
+const reset=document.querySelector(".reset");
+reset.addEventListener('click',resetGrid);
 
 const grids=document.querySelectorAll(".grid");
 grids.forEach(grid=>grid.addEventListener('mouseenter',colorGrid));
