fd9ef3b3b57e0c19c59ecbae7804a93e45f5692b
anixudh <anirudh.varma196@gmail.com>
2021-09-15 13:52:50 +0530

    made the box grids size responsive to the new size

--- myscript.js
+++ myscript.js
@@ -1,11 +1,17 @@
 const gridbox=document.querySelector(".gridbox");
 
 const createNewGrid=(n)=>{
+
+    if(n>100) n=100;
+    let width=480/n + "px";
+
     for(let i=0;i<n;i++){
         const gridRow=document.createElement("div");
         for(let j=0;j<n;j++){
             const grid=document.createElement("div");
             grid.classList.add("grid");
+            grid.style.width=width;
+            grid.style.height=width;
             gridRow.appendChild(grid);
         }
         gridRow.classList.add("gridrow");
@@ -15,7 +21,7 @@ const createNewGrid=(n)=>{
 
 const resetGrid=e=>{
     const gridrows=document.querySelectorAll(".gridrow");
-    const newSize=prompt("Enter new grid size");
+    const newSize=prompt("Enter new grid size (max:100)");
     gridrows.forEach(gridRow=>gridbox.removeChild(gridRow));
     createNewGrid(newSize);
     const grids=document.querySelectorAll(".grid");
